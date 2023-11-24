58b35cdb73dbda6ddbb998ca2db6f75222830021
Casvill <casvilldaniel@gmail.com>
2023-11-24 15:56:02 -0500

    Java-Postgres Interface created

---
+++ src/controller/ControladorOrden.java
@@ -0,0 +1,23 @@
+package controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ *
+ * @author Daniel Casvill
+ */
+public class ControladorOrden 
+{
+    
+    class OrdenListener implements ActionListener
+    {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            
+        }
+        
+    }
+    
+}
