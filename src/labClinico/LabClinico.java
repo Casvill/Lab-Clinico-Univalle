e8167644655b06b46ada61ca972c5069cc4b8c28
Casvill <casvilldaniel@gmail.com>
2023-11-24 01:03:20 -0500

    First Commit

---
+++ src/labClinico/LabClinico.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package labClinico;
+
+import view.ViewMain;
+
+/**
+ *
+ * @author Daniel Casvill
+ */
+public class LabClinico 
+{
+    public static void main(String[] args) 
+    {
+        // TODO code application logic here
+        ViewMain main = new ViewMain();
+        main.setVisible(true);
+        main.setLocationRelativeTo(null);
+    }
+    
+}
